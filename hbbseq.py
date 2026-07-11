3e12f9380d70f175ee239997646e9b083a55fd50
Akansha_Ghosh_bioinfo <akansha11ghosh@gmail.com>
2026-07-11 13:44:41 +0530

    Update hbbseq.py

--- hbbseq.py
+++ hbbseq.py
@@ -5,19 +5,50 @@ print(record.description)
 print(len(record))
 print(record.seq)
 
+#parsing the FASTA file
 for seq_record in SeqIO.parse("hbbseq.fasta", "fasta"):
     seq=str(seq_record.seq)
-    g_count=seq.count("G")
-    c_count=seq.count("C")
-    gc_count=(seq.count("G")+seq.count("C"))/len(seq)*100
-
+    
     a_count=seq.count("A")
     t_count=seq.count("T")
-    at_count=(seq.count("A")+seq.count("T"))/len(seq)*100
+    at_count_percentage=(seq.count("A")+seq.count("T"))/len(seq)*100
 
-print(g_count)
-print(c_count)
-print(gc_count)
+    g_count=seq.count("G")
+    c_count=seq.count("C")
+    gc_count_percentage=(seq.count("G")+seq.count("C"))/len(seq)*100
+
+
 print(a_count)
 print(t_count)
-print(at_count)
+print(at_count_percentage)
+print(g_count)
+print(c_count)
+print(gc_count_percentage)
+
+# for creating pie chart
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+bases = ["Adenine", "Thymine", "Guanine", "Cytosine"]
+
+
+composition = [22068, 22309, 14785, 14146]
+
+
+colors = ["Pink", "Cyan", "Lightgreen", "Orange"]
+
+
+plt.pie(composition,
+        labels=bases,
+        colors=colors,
+        autopct="%1.1f%%",
+        startangle=90)
+
+
+plt.title("Composition of Nitrogenous Bases")
+
+plt.show()
+plt.title("Composition of Nitrogenous Bases")
+
+plt.show()
